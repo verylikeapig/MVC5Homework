a6f176f42466dec0f9330e4ea5663435ab873b72
Raymond <verylikeapig@hotmail.com>
2018-01-26 00:13:50 +0800

    增加ViewModel的頁面，並加入輸入欄位的attribute。

--- Day1Homework/Day1Homework/Controllers/HomeController.cs
+++ Day1Homework/Day1Homework/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Day1Homework.BL;
+using Day1Homework.ViewModels.Home;
 
 namespace Day1Homework.Controllers  // 調度資源和組裝ViewModel
 {
@@ -17,12 +18,12 @@ namespace Day1Homework.Controllers  // 調度資源和組裝ViewModel
             return View(data);
         }
 
-        public ActionResult MyAccountBook()
+        public ActionResult MyAccountBook(MoneyDetailViewModel viewModel)
         {
             ViewBag.Title = "我的記帳本";
             ViewBag.Message = "收入與支出 - Child Action";
 
-            return View();
+            return View(viewModel);
         }
 
         [ChildActionOnly]
--- Day1Homework/Day1Homework/ViewModels/Home/MoneyDetailViewModel.cs
+++ Day1Homework/Day1Homework/ViewModels/Home/MoneyDetailViewModel.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Day1Homework.Models.Home
+namespace Day1Homework.ViewModels.Home
 {
     public class MoneyDetailViewModel
     {
+        [Display(Name = "類別")]
+        public string Category { get; set; }
+
+        [Required]
+        [Display(Name = "金額")]
+        [Range(0, 2147483647, ErrorMessage = "數值區間 0 ~ 2,147,483,647")]
+        public decimal Money { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime Date { get; set; }
+
+        [Display(Name = "備註")]
+        [StringLength(500, ErrorMessage = "最大字數是500字，最小字數是3字。", MinimumLength = 3)]
+        public string Description { get; set; }
     }
 }
